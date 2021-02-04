dbb63cc2fbd7385d239585873bf23a6a0ec34962
Audy0714 <audykei7907@gmail.com>
2021-02-04 14:51:58 +0100

    model Boardgame

---
+++ app/database.js
@@ -0,0 +1,12 @@
+
+const { Pool } = require('pg');
+
+// ici, les informations de connection sont récupérées dans l'environnement
+// PGHOST pour l'hôte
+// PGUSER pour l'utilisateur
+// PGPASSWORD pour le mot de passe
+// PGDATABASE pour la base de données
+const db = new Pool();
+
+// maintenant on a plus un seul connecteur mais un pool de connecteur
+module.exports = db;
---
+++ app/models/boardgame.js
@@ -0,0 +1,58 @@
+const db = require('../database');
+
+class Boardgame {
+    id;
+    name;
+    // camelCase ici, snake_case côté BDD
+    minAge;
+    minPlayers;
+    maxPlayers;
+    duration;
+    creator;
+
+    /*
+        setters (accesseurs)
+
+        appeler :
+        this.min_players = 4
+        déclenchera :
+        this.minPlayers = 4
+    */
+
+    set min_players(val) {
+    this.minPlayers = val;
+    }
+
+    set max_players(val) {
+    this.maxPlayers = val;
+    }
+
+    set min_age(val) {
+    this.minAge = val;
+    }
+
+
+
+    constructor(data = {}) {
+        for (const prop in data) {
+            this[prop] = data[prop];
+        }
+    }
+
+    // méthode static sur une class donc ici Boardgame, on pourra appeler Boargame.findAll()
+    static async findAll() {
+
+        const { rows } = await db.query('SELECT * FROM boardgame;');
+
+        return rows.map(game => new Boardgame(game));
+    }
+
+    static async findOne(id) {
+
+        const { rows } = await db.query('SELECT * FROM boardgame WHERE id = $1;', [id]);
+
+        return new Boardgame(rows[0]);
+    }
+}
+
+module.exports = Boardgame;
