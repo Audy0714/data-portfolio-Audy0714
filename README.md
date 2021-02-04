a682614687bbe13483c235b34aee3971fac7d457
Audy0714 <audykei7907@gmail.com>
2021-02-04 13:41:34 +0100

    début du projet

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# data-portfolio
+# Portfolio jeux de société
+
+<description>
+
+## Stack
+
+- Node 10+
+  - Express
+  - Joi
+- PostgreSQL 11+
+  - Sqitch
+
+## Structure des données
+
+<mcd>
---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const express = require('express');
+
+//const pg = require('pg');
+
+const app = express();
+
+const port = process.env.PORT || 5478;
+
+
+// ici, on pourrait aussi écrire notre 404
+
+app.listen(port, () => { console.log(`Listening on http://localhost:${port}`) });
+
